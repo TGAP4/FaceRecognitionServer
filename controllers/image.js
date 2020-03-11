f0a26cdcb18536a8475917285a26ed3d0ec7933c
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-03-11 04:04:14 -0400

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -1,13 +1,13 @@
 const Clarifai = require('clarifai');
 
-const app = new Clarifai.App({apiKey: process.ENV.API_KEY});
+const app = new Clarifai.App({apiKey: process.env.API_KEY});
 
 const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
         res.json(data);
     })
-    .catch(erro => res.status(400).json('Unable to work with API'));
+    .catch(err => res.status(400).json('Unable to work with API'));
 }
 
 const handleImage = (req, res, db) => {
